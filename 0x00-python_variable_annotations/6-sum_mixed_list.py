1d35df43d5e44bc19991585fba294ad587b2bf6d
Jonathan Boomni <rejoiceoye1@gmail.com>
2023-08-04 10:29:17 +0100

    Wrote some python type annotations

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python3
 
 """ mixed list """
-from typing import List
+from typing import List, Union
 
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """
+    Calculate the sum of integers and floats in the mixed list.
 
-def sum_mixed_list(mxd_lst: List[int | float]) -> float:
-    """  returns sum as a float. """
+    Parameters:
+        mxd_lst (List[Union[int, float]]): A list containing integers and floats.
+
+    Returns:
+        float: The sum of the integers and floats in the list as a float.
+    """
     return float(sum(mxd_lst))
--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 """ Complex types - string and int/float to tuple"""
-from typing import Callable, Iterator, Optional, List, Tuple
+from typing import Union, Tuple
 
-
-def to_kv(k: str, v: int | float) -> Tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
-    takes a string k and an int OR float v as arguments
-    returns a tuple.
-    """
+    Convert a string-key and int/float-value pair to a tuple with the square of the value as a float.
 
+    Parameters:
+        k (str): The string key.
+        v (Union[int, float]): The integer or float value.
+
+    Returns:
+        Tuple[str, float]: A tuple containing the string key and the square of the value as a float.
+    """
     return (k, v**2)
