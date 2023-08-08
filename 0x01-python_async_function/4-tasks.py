3ee6b917ce6782943094509bfb2cd903700b0870
Jonathan Boomni <rejoiceoye1@gmail.com>
2023-08-08 04:07:46 +0100

    Updated task_wait_random to function correctly

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -16,5 +16,7 @@ async def task_wait_n(n: int, max_delay: int) -> List[float]:
     Returns delays in ascending order
     """
     coroutines = [task_wait_random(max_delay) for _ in range(n)]
-    result = await asyncio.gather(*coroutines)
-    return sorted(result)
+    results = []
+    for task in asyncio.as_completed(coroutines):
+        results.append(await task)
+    return results
