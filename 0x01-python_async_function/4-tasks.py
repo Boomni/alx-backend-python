dc0bb6dc0e22504fd464b493b2c4a7451446995b
Jonathan Boomni <rejoiceoye1@gmail.com>
2023-08-08 03:32:31 +0100

    Updated task_wait_n

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -11,8 +11,6 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 async def task_wait_n(n: int, max_delay: int) -> list:
     """Spawns wait n times with the spacified max_delay"""
-    result: list = []
-    for x in range(n):
-        delay: float = await task_wait_random(max_delay)
-        result.append(delay)
-    return result
+    coroutines = [task_wait_random(max_delay) for _ in range(n)]
+    result = await asyncio.gather(*coroutines)
+    return sorted(result)
