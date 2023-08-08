e15d52cff8d41852ee666c05d36557aa0641bbef
Jonathan Boomni <rejoiceoye1@gmail.com>
2023-08-08 03:40:30 +0100

    Updated measure_time to use perf_counter function from time module to measure the approx elapsed time

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -16,9 +16,9 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
-    start_time = time.time()
+    start_time = time.perf_counter()
     asyncio.run(wait_n(n, max_delay))
-    end_time = time.time()
+    end_time = time.perf_counter()
     total_time = end_time - start_time
     average_time = total_time / n
     return float(average_time)
