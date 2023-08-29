12bd375f7abfd735b8196507fb367d377e7521b3
Boomni Jonathan <rejoiceoye1@gmail.com>
2023-08-29 18:50:12 +0100

    Start of the project

---
+++ 0x03-Unittests_and_integration_tests/README.md
@@ -0,0 +1,1 @@
+Unittests ans integration tests
---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Unit test for utils.access_nested_map"""
+import unittest
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """This class inherits from unittest.TestCase"""
+
+    def test_access_nested_map():
+        pass
